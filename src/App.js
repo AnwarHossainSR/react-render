cb5970ee02660fd20361ecc6dbe9c53f071efc38
AnwarHossainSR <mahedianwar@gmail.com>
2022-03-28 11:59:58 +0600

    Parent and Child behavior

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { Parent } from "./components/Parent Child/Parent";
 //import { UseReducer } from "./components/UseReducer/UseReducer";
 //import { UseState } from "./components/UseState/UseState";
 //import { ObjectUseState } from './components/Immutable State/ObjectUseState'
-import { ArrayUseState } from "./components/Immutable State/ArrayUseState";
+//import { ArrayUseState } from "./components/Immutable State/ArrayUseState";
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       {/* <UseState />
       <UseReducer /> */}
       {/* <ObjectUseState /> */}
-      <ArrayUseState />
+      {/* <ArrayUseState /> */}
+      <Parent />
     </div>
   );
 };
