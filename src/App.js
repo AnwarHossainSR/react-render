e6c2bdac42a576e16c6f1139e2714056944092b7
AnwarHossainSR <mahedianwar@gmail.com>
2022-03-28 11:54:14 +0600

    immutable array state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 //import { UseReducer } from "./components/UseReducer/UseReducer";
 //import { UseState } from "./components/UseState/UseState";
-import { ObjectUseState } from './components/Immutable State/ObjectUseState'
+//import { ObjectUseState } from './components/Immutable State/ObjectUseState'
+import { ArrayUseState } from "./components/Immutable State/ArrayUseState";
 
 const App = () => {
   return (
     <div>
       {/* <UseState />
       <UseReducer /> */}
-      <ObjectUseState />
+      {/* <ObjectUseState /> */}
+      <ArrayUseState />
     </div>
   );
 };
