cd7697d18c8047885c0667e24a9fb8be8952af09
AnwarHossainSR <mahedianwar@gmail.com>
2022-03-28 11:51:06 +0600

    object immutability

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 //import { UseReducer } from "./components/UseReducer/UseReducer";
 //import { UseState } from "./components/UseState/UseState";
+import { ObjectUseState } from './components/Immutable State/ObjectUseState'
 
 const App = () => {
   return (
     <div>
       {/* <UseState />
       <UseReducer /> */}
+      <ObjectUseState />
     </div>
   );
 };
