8c9dbdacc1f4aa40339eac349d8bc4d954801f3a
AnwarHossainSR <mahedianwar@gmail.com>
2022-03-28 12:16:48 +0600

    Optimization :  Same Element Reference

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { Parent } from "./components/Parent Child/Parent";
+import { GrandParent } from "./components/Optimization/GrandParent";
+//import { ChildOne } from "./components/Optimization/ChildOne";
+//import { ParentOne } from "./components/Optimization/ParentOne";
+//import { Parent } from "./components/Parent Child/Parent";
 //import { UseReducer } from "./components/UseReducer/UseReducer";
 //import { UseState } from "./components/UseState/UseState";
 //import { ObjectUseState } from './components/Immutable State/ObjectUseState'
@@ -12,7 +15,11 @@ const App = () => {
       <UseReducer /> */}
       {/* <ObjectUseState /> */}
       {/* <ArrayUseState /> */}
-      <Parent />
+      {/* <Parent /> */}
+      {/* <ParentOne>
+        <ChildOne />
+      </ParentOne> */}
+      <GrandParent />
     </div>
   );
 };
